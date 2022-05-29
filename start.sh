6a83d20bd5259207936e0223ddb7aabd03b4d29b
urevoleg <urevolegg@gmail.com>
2022-05-29 22:17:48 +0300

    fix

--- start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /home/userad/projects/ya-tracker-dash && source venv/bin/activate
-airflow webserver --port 8080 && airflow scheduler
---
+++ start_airflow.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /home/userad/projects/airflow-tutorial && source venv/bin/activate
+airflow webserver --port 8080
